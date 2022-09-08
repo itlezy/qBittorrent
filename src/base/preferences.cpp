7af87ec8a6d3743efed5af950ce8d8da8e5cb115
itLezy <itLezy@users.noreply.github.com>
2022-09-08 10:16:13 +0200

    Comments on preferences

--- src/base/preferences.cpp
+++ src/base/preferences.cpp
@@ -1669,42 +1669,66 @@ void Preferences::setSpeedWidgetGraphEnable(const int id, const bool enable)
     setValue(QString::fromLatin1("SpeedWidget/graph_enable_%1").arg(id), enable);
 }
 
+/**
+* Behavior upon a DHT get_peers request on completed torrent
+* */
 int Preferences::getResumeUponGetPeersBehavior() const
 {
     // 0 off, 1 standard resume, 2 forced resume
     return value("Extensions/ResumeUponGetPeers/Behavior", 2);
 }
 
+/**
+* The min free Gb
+* */
 int Preferences::getLowDiskSpaceCheckGb() const
 {
     return value("Extensions/LowDiskSpaceCheck/Gb", 12);
 }
 
+/**
+* The path to monitor for low disk space, the default is the download path from settings
+* */
 QString Preferences::getLowDiskSpaceCheckPathToMonitor() const
 {
     return value<QString>("Extensions/LowDiskSpaceCheck/PathToMonitor");
 }
 
+/**
+* The upload speed to set when disk space gets low
+* */
 int Preferences::getLowDiskSpaceCheckUpSpeed() const
 {
     return value("Extensions/LowDiskSpaceCheck/UpSpeed", 1024 * 1024);
 }
 
+/**
+* The download speed to set when disk space gets low
+* */
 int Preferences::getLowDiskSpaceCheckDownSpeed() const
 {
     return value("Extensions/LowDiskSpaceCheck/DownSpeed", 16 * 1024);
 }
 
+/**
+* Automatic switch between normal and alternate bandwitdh
+* */
 bool Preferences::isDynamicBandwidthSwitchEnabled() const
 {
     return value("Extensions/ApplyDynamicBandwidthSwitch/Enabled", true);
 }
 
+/**
+* Automatic switch to alternate bandwidth when download falls below this threshold
+* */
 int Preferences::getDynamicBandwidthSwitchLowerThreshold() const
 {
     return value("Extensions/DynamicBandwidthSwitch/LowerThreshold", 256 * 1024);
 }
 
+/**
+* What to do when torrents are stalled in download
+* */
 int Preferences::getStalledDownloadBehavior() const
 {
     // 0 off, 1 pause, 2 forced download
